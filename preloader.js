7c78b62928d988d07d1171a72e9599b79c5a6b6b
Matheus Oliveira <53400866+matheustkful@users.noreply.github.com>
2025-10-18 19:35:16 -0300

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -22,7 +22,7 @@ function hideOverlay() {
         renderer: 'svg', // ou 'canvas' se preferir
         loop: true,
         autoplay: true,
-        path: 'https://matheustkful.github.io/organizaLottie/organiza.json',
+        path: 'https://matheustkful.github.io/organizaLottie/organiza (2).json',
     });
 }
 
